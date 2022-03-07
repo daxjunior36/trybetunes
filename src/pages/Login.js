b4dad49772e6bc37dad4b82270a953b28369cc65
Domingos Alexandre <domingos_alexandre@d.alexandre.jrInspiron-3501>
2022-03-07 18:54:11 -0300

    requisito 01

---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <div data-testid="page-login"> Login </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
