d13341007f95c2f600f88ddd99c103c60c5dad29
Domingos Alexandre <domingos_alexandre@d.alexandre.jrInspiron-3501>
2022-03-09 21:03:11 -0300

    requisito 04

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
@@ -32,6 +33,9 @@ class Header extends React.Component {
         <header data-testid="header-component">
           { loanding === true ? <p>Carregando...</p> : (
             <p data-testid="header-user-name">{ retorna }</p>)}
+          <Link data-testid="link-to-search" to="/search"> Search </Link>
+          <Link data-testid="link-to-favorites" to="/favorites"> Favorites </Link>
+          <Link data-testid="link-to-profile" to="/profile"> Profile </Link>
         </header>
 
       );
